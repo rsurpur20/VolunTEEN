82a64c9f68b1e537875dbdfdeecd3e1baebc989b
rsurpur20 <42478922+rsurpur20@users.noreply.github.com>
2020-08-11 15:08:46 -0400

    Tuesday end of class
    what we did:
    created storyboard with navigation view control, and two table view controllers, and main controller page. segues throughout all expect the add button
    all are connected to their own class files
    we are on step b of iteration 0

---
+++ VolunTEEN/VolunTEEN/VolunTEEN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VolunTEEN
+//
+//  Created by Roshni Surpur on 8/11/20.
+//  Copyright © 2020 Roshni Surpur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VolunTEEN/VolunTEEN/VolunTEEN/VolunteerClass.swift
@@ -0,0 +1,19 @@
+//
+//  VolunteerClass.swift
+//  VolunTEEN
+//
+//  Created by Roshni Surpur on 8/11/20.
+//  Copyright © 2020 Roshni Surpur. All rights reserved.
+//
+
+import UIKit
+
+
+class VolunteerClass {
+    var title = ""
+//    var link = ""
+   var description = ""
+   var remote = false
+
+
+}
